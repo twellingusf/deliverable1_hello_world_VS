86ecfd0ab7c311dca77f349c6cc40c98c3431929
Trevo <Trevo@DESKTOP-DCC7DMS>
2023-01-14 20:12:15 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace deliverable1_hello_world_VS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Solution (1): Hello, World!");
+            Console.WriteLine(" ");
+
+            string stg = "Hello, World!";
+            Console.WriteLine("Solution (2): " + stg);
+            Console.WriteLine(" ");
+
+            Console.WriteLine("Enter a username: ");
+                string userName = Console.ReadLine();
+            Console.WriteLine("The user's name is: "+ userName);
+
+            Console.WriteLine("Enter the user's age: ");
+            int userAge = int.Parse(Console.ReadLine());
+            Console.WriteLine("The user's age is: " + userAge);
+        }
+    }
+}
